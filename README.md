340109addd8595facb1ef614343f36ab29098f4c
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-01 20:59:06 +0100

    subiendo toda la estructura

---
+++ README.md
@@ -0,0 +1,9 @@
+# TRITURA-DATOS
+
+Muchos de los datasets traen información que por diversos motivos no sirve.
+
+La trituradora sigue el ejemplo de [Analyzing Data with Node
+](http://learnjsdata.com/node.html)
+
+
+
---
+++ trituradora.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+var d3 = require("d3");
+var _ = require("lodash");
+
+//Cargamos el dataset
+fs.readFile("data/distritos.csv", "utf8", function(error, data) {
+
+    data = d3.csvParse(data);
+
+    var votoFilter = data.filter(function(d) {
+        return d.poblacion > 40000;
+    });
+
+    votoFilterClean = d3.csvFormat(votoFilter);
+
+    fs.writeFile("data-clean/distritos.csv", votoFilterClean, function(err) {
+        console.log("file written");
+    });
+
+});
