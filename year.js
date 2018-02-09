be7fb4c95481815d0d08af18e74f69f4a1cb99b7
Jorge Aznar <jorgeatgu@gmail.com>
2018-02-09 16:40:45 +0100

    data-mestika triturado :neckbeard:

--- year.js
+++ year.js
@@ -3,14 +3,13 @@ var d3 = require("d3");
 var _ = require("lodash");
 
 //Cargamos el dataset
-fs.readFile("data-mestika/datos.csv", "utf8", function(error, data) {
+fs.readFile("data-mestika/data-ux.csv", "utf8", function(error, data) {
 
     data = d3.csvParse(data);
 
-    //Filtrar las de UX
-    data = data.filter(function(item) {
-        return item.ciudad ? item.ciudad.match(/Barcelona/gi) : false;
-    });
+    function getYear(stringDate) {
+        return stringDate.split('-')[4];
+    }
 
     //Prouuection de solo las 3 keys que queremos ASAP
     data = data.map(function(item){
@@ -21,12 +20,10 @@ fs.readFile("data-mestika/datos.csv", "utf8", function(error, data) {
         }
     });
 
-    //Descartamos las ciudades que tienen menos de 11 ofertas publicadas
-    // var data = grupoCiudad.filter(function (d) { return d.value > 10})
 
     var csv = d3.csvFormat(data);
 
-    fs.writeFile("data-mestika-clean/data-barcelona.csv", csv, function(err) {
+    fs.writeFile("data-mestika-clean/data-ux.csv", csv, function(err) {
         console.log("Hecho!!");
     });
 
